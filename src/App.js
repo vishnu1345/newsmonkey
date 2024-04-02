bfc0ddc0655b853bfd0633c75d535fc74566e23a
vishnu <142923987+vishnu1345@users.noreply.github.com>
2024-04-02 11:08:47 +0530

    started project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Home from './Components/Home';
+import About from './Components/About';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element:   <>
+      <Home/>
+       </>,
+    
+    },
+    {
+      path: "/about",
+      element:  <About/>
+    }
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <RouterProvider router={router} />
+   </>
   );
 }
 
